db239217309aac4cb70026155aad3c677bab9b12
Nova <150054754+ZoraNovachrono@users.noreply.github.com>
2025-12-09 08:57:31 +0100

    Add NavMeshPlus 2D navigation components and editor scripts
    Introduces NavMeshPlus 2D navigation system, including runtime scripts and custom editor scripts for agent overrides, source collection, caching, links, modifiers, and surfaces. Updates SurvivorSceneTest.unity and adds a NavMesh asset to the scene for navigation support.

--- Assets/Scripts/Survivor/Survivor.cs
+++ Assets/Scripts/Survivor/Survivor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.AI;
 
 public class Survivor : MonoBehaviour
 {
@@ -10,18 +11,34 @@ public class Survivor : MonoBehaviour
     [SerializeField]
     private float detectorRange;
 
+    private bool isCured = false;
+    NavMeshAgent agent;
+
     private void Start()
     {
         inputActions = new PlayerInputActions();
         inputActions.Player.Enable();
         inputActions.Player.Interact.performed += InjectSereum;
-        
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        isCured = false;
+    }
+    private void Update()
+    {
+        if (isCured)
+        {
+            agent.SetDestination(player.transform.position);            
+        }
     }
     public void InjectSereum(InputAction.CallbackContext ctx)
     {
         if (player.layer == gameObject.layer && Vector3.Distance(player.transform.position, transform.position) <= detectorRange)
         {
             Debug.Log("Inject Sereum");
+            isCured = true;
         }
     }
 
